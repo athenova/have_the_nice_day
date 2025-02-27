b00aa20452be633847f3e565ee28deab09133853
athenova <aleksey.sergeyev@yandex.com>
2025-02-27 21:57:45 +0300

    Переход на deepseek

--- have_the_nice_day.py
+++ have_the_nice_day.py
@@ -4,28 +4,32 @@ import schedule
 import time
 from openai import OpenAI
 
-AI_TEXT_MODEL = 'gpt-4o-mini'
+AI_TEXT_MODEL = 'deepseek-chat'
+CHAT_TOKEN_NAME = "DEEPSEEK_API_KEY"
 BOT_TOKEN_NAME = "ATHE_BOT_TOKEN"
 BOT_TOKEN = os.environ.get(BOT_TOKEN_NAME)
-# CHAT_ID = -1002374309134
+#CHAT_ID = -1002374309134
 CHAT_ID = '@have_the_nice_day'
 
 def job():
-    client = OpenAI()
+    client = OpenAI(api_key=os.environ.get(CHAT_TOKEN_NAME), base_url="https://api.deepseek.com")
     text = client.chat.completions.create(
         model=AI_TEXT_MODEL,
         messages=[
             { "role": "system", "content": f"Ты - самый оптимистичный в мире человек" },
-            { "role": "user", "content": f"Пожелай отличного дня всему миру, используй смайлики" },
+            { "role": "user", "content": f"Пожелай отличного дня всему миру, используй смайлики, не используй 'Конечно'" },
         ]
     ).choices[0].message.content
     bot = telebot.TeleBot(BOT_TOKEN)
     bot.send_message(chat_id=CHAT_ID, text=text, parse_mode="Markdown")
 
-schedule.every().day.at("07:00",'Europe/Moscow').do(job)
+#job()
 
-fifteen_minutes = 15 * 60
+if __name__ == "__main__":
+    schedule.every().day.at("07:00",'Europe/Moscow').do(job)
 
-for i in range(fifteen_minutes):
-    schedule.run_pending()
-    time.sleep(1)
+    fifteen_minutes = 15 * 60
+
+    for i in range(fifteen_minutes):
+        schedule.run_pending()
+        time.sleep(1)
